4cc097ac6181840aed3359df6ad829006483825c
lenover-p <16PL16@queensu.ca>
2019-11-19 16:40:41 -0500

    Made name fields required when creating user

--- backend/studii/posts/urls.py
+++ backend/studii/posts/urls.py
@@ -8,8 +8,6 @@ router.register(r'courses', CourseView)
 router.register(r'posts', PostView)
 
 urlpatterns = [
-    #url(r'courses/', Courses.as_view(), name='courses'),
-    #url(r'posts/', posts.as_view(), name='post'),
     url(r'^', include(router.urls)),
     url(r'upload/', ContentUploadView.as_view(), name='content_upload'),
 ]
