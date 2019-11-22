a0c58061edcd3c189396886c0dd1b45e2d1ed78a
lenover-p <16PL16@queensu.ca>
2019-11-21 23:29:09 -0500

    Added API view to serve all posts for given course

--- backend/studii/posts/urls.py
+++ backend/studii/posts/urls.py
@@ -1,13 +1,14 @@
-from posts.views import ContentUploadView, CourseView, PostView
+from posts.views import ContentUploadView, CourseView, PostView, ListPostsView
 from django.conf.urls import url
 from django.urls import include, path
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'courses', CourseView)
-router.register(r'posts', PostView)
+router.register(r'content', PostView)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'upload/', ContentUploadView.as_view(), name='content_upload'),
+    url(r'^show/(?P<pk>\d+)/$', ListPostsView.as_view(), name='Posts'),
 ]
